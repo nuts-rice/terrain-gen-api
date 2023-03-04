891b96b5a3179ea197911a8773bbbb88ebae75ed
mishka_root <mishka_root@0x0f-softworks-librem.lan>
2023-03-04 17:08:37 -0500

    🔊 logging

--- tests/test.rs
+++ tests/test.rs
@@ -1,8 +1,30 @@
 #[cfg(test)]
 mod tests {
+
     use std::net::TcpListener;
 
+    use tracing::subscriber::set_global_default;
+    use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+    use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
+
+    /*
+        let stdout_log = tracing_subscriber::fmt::layer().pretty();
+
+        let file: File::create("debug.log")?;
+        let debug_log = tracing_subscriber::fmt::layer()
+            .with_writer(Arc::new(file))
+    */
+
     fn spawn_app() -> String {
+        let env_filter =
+            EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+        let fmt_layer = BunyanFormattingLayer::new("terrain-gen-api".into(), std::io::stdout);
+        let subscriber = Registry::default()
+            .with(env_filter)
+            .with(JsonStorageLayer)
+            .with(fmt_layer);
+        set_global_default(subscriber)
+            .expect("oh no you posted cringe S: now you will lose subscriber ");
         let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
         let port = listener.local_addr().unwrap().port();
         let server = terrain_gen_api::startup::run(listener).expect("failed to bind address");
@@ -10,11 +32,16 @@ mod tests {
         let _ = tokio::spawn(server);
         format!("http://127.0.0.1:{}", port)
     }
+
     #[actix_rt::test]
     async fn health_check_test() {
+        //        tracing_subscriber::fmt()
+        //            .with_max_level(tracing::Level::INFO)
+        //            .init();
+
         let address = spawn_app();
         let client = reqwest::Client::new();
-
+        tracing::info!("client requesting health check...");
         let response = client
             .get(&format!("{}/health_check", &address))
             .send()
@@ -23,6 +50,7 @@ mod tests {
 
         assert!(response.status().is_success());
         assert_eq!(Some(0), response.content_length());
+        tracing::info!("health check sucessful");
     }
 
     #[actix_rt::test]
