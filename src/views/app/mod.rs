8b510a5ceea34cbbd05b21e5a822f084b86bb032
nuts-rice <red44443@gmail.com>
2023-04-10 13:14:22 -0400

    🎨 web scripts

--- src/views/app/mod.rs
+++ src/views/app/mod.rs
@@ -1,7 +1,6 @@
 use actix_web::web;
 mod heightmap_items;
 
-
 pub fn heightmap_app_views_factory(app: &mut web::ServiceConfig) {
     app.route("/height_map_view", web::get().to(heightmap_items));
 }
