8a9a7af2baace74ef06459d58346f421d19bd1c7
nuts-rice <red44443@gmail.com>
2023-04-11 15:12:27 -0400

    🏷️ heightmap size and spread types and reorg

--- src/domain/new_heightmap.rs
+++ src/domain/new_heightmap.rs
@@ -34,7 +34,25 @@ impl AsRef<usize> for HeightmapSize {
 #[derive(Debug)]
 pub struct HeightmapSpreadRate(f64);
 impl HeightmapSpreadRate {
-    pub fn parse(_f: f64) -> Result<HeightmapSpreadRate, String> {
-        unimplemented!()
+    pub fn parse(u: &str) -> Result<HeightmapSpreadRate, String> {
+        let is_empty_or_whitespace = u.trim().is_empty();
+        let is_too_long = u.graphemes(true).count() > 256;
+        let forbidden_characters = ['/', '(', ')', '"', '<', '>', '\\', '{', '}'];
+        let contains_forbidden_characters = u.chars().any(|g| forbidden_characters.contains(&g));
+        let value = match u.parse::<f64>() {
+            Ok(value) => value,
+            Err(_) => todo!(),
+        };
+        if is_empty_or_whitespace || is_too_long || contains_forbidden_characters {
+            Err(format!("{} is not a valid heightmap size.", u))
+        } else {
+            Ok(Self(value))
+        }
+    }
+}
+
+impl AsRef<f64> for HeightmapSpreadRate {
+    fn as_ref(&self) -> &f64 {
+        &self.0
     }
 }
--- src/javascript/index.js
+++ src/javascript/index.js
@@ -7,3 +7,13 @@ async function run() {
   const canvas = document.getElementById('heightmapCanvas');
   const ctx = canvas.getContext('2d');
 }
+
+function renderHeightmap(_heightmap, processType, elementId, processFunction) {
+  let itemsMeta = []
+  let placeholder = "<div>"
+  for (let i = 0; i < _heightmap.length; i++) {
+    . . .
+  }
+  placeholder += "</div>"
+  document.getElementById(elementId).innerHTML = placeholder;
+}
